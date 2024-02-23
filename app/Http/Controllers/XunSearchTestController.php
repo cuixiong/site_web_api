a6d2503f01a77c779ec8881ac5d57d5dd3996d21
小宝 <1192063282@qq.com>
2024-02-23 17:30:32 +0800

    完善BUG

--- app/Http/Controllers/XunSearchTestController.php
+++ app/Http/Controllers/XunSearchTestController.php
@@ -2,16 +2,15 @@
 
 namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
-use App\Models\SystemValue;
+use App\Http\Helper\XunSearch;
 use Illuminate\Http\Request;
-use XS;
 
 class XunSearchTestController extends Controller
 {
     public function clean(Request $request)
     {
-        $xs = new XS('/www/wwwroot/www.marketmonitorglobal.com.cn/api/config/xunsearch/MMG_CN.ini');
-        $index = $xs->index;
-        $index->clean();
+        $xs = new XunSearch();
+        $xs->clean();
+        echo "完成".date('Y-m-d H:i:s',time());
     }
 }
