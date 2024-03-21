10d2b179201eee2473057aad28c38caa9cd1124d
小宝 <1192063282@qq.com>
2024-03-21 17:44:40 +0800

    ...

--- app/Models/Products.php
+++ app/Models/Products.php
@@ -32,7 +32,7 @@ class Products extends Base
             $timestamp = time();
         }
         $actuallyPaid = $price;
-        if ($timestamp >= $goods['discount_begin'] && $timestamp <= $goods['discount_end']) { // 如果队列不能把discount_begin和discount_end的值恢复成null，就不能要这句代码了
+        if ($timestamp >= $goods['discount_time_begin'] && $timestamp <= $goods['discount_time_end']) { // 如果队列不能把discount_time_begin和discount_time_end的值恢复成null，就不能要这句代码了
             if ($goods['discount_type'] == 1) {
                 $actuallyPaid = $price * $goods['discount'] / 100;
             } else if ($goods['discount_type'] == 2) {
