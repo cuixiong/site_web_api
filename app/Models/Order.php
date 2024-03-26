b2f7990d07ffc1a6076dfc72916bdb464621d1c6
小宝 <1192063282@qq.com>
2024-03-26 13:37:08 +0800

    ..

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -10,7 +10,7 @@ class Order extends Base
     {
         static $payType = null;
         if ($payType === null) {
-            $payment = Payment::select('id', 'code')->get()->toArray();
+            $payment = Pay::select('id', 'code')->get()->toArray();
             $payType = [];
             for ($i = 0, $len = count($payment); $i < $len; $i++) {
                 $payType[$payment[$i]['id']] = $payment[$i]['code'];
