0585ca1f6824334a83343679a75887d2bea10beb
zqy <695326913@qq.com>
2024-04-07 11:19:03 +0800

    字典

--- app/Models/DictionaryValue.php
+++ app/Models/DictionaryValue.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Models;
+
 use App\Models\Base;
 use Illuminate\Support\Facades\Redis;
 
@@ -9,16 +10,16 @@ class DictionaryValue extends Base
     // 获取某一个code字典的全部选项
     public static function GetDicOptions($code)
     {
-        $list = Redis::hGetAll('dictionary_'.$code);
+        $list = Redis::hGetAll('dictionary_' . $code);
         $result = [];
-        if($list){
+        if ($list) {
             foreach ($list as $map) {
-                $map = json_decode($map,true);
-                if($map['status'] == 1){
-                    $result[] = ['value' => $map['value'],'label' => $map['name'],'sort' => $map['sort'] ?? 0];
+                $map = json_decode($map, true);
+                if ($map['status'] == 1) {
+                    $result[] = ['value' => $map['value'], 'label' => $map['name'], 'sort' => $map['sort'] ?? 0];
                 }
             }
-            self::sortBySort($result,'sort');
+            self::sortBySort($result, 'sort');
         }
         return $result;
     }
@@ -31,12 +32,13 @@ class DictionaryValue extends Base
      * @param int $order 可选，排序顺序，1表示升序，-1表示降序
      * @return void 直接修改原数组
      */
-    public static function sortBySort(&$arr,$key,$order = SORT_ASC){
-        usort($arr,function($a,$b) use ($key,$order) {
-            if(!isset($a[$key]) || !isset($b[$key])){
+    public static function sortBySort(&$arr, $key, $order = SORT_ASC)
+    {
+        usort($arr, function ($a, $b) use ($key, $order) {
+            if (!isset($a[$key]) || !isset($b[$key])) {
                 return false;
             }
-            $result = strcmp($a[$key],$b[$key]);
+            $result = strcmp($a[$key], $b[$key]);
             return ($order == SORT_ASC) ? $result : -$result;
         });
     }
