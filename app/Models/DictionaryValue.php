81fbc17984862900537003c19aca23c934669f09
zqy <695326913@qq.com>
2024-04-11 18:13:12 +0800

    字典获取接口

--- app/Models/DictionaryValue.php
+++ app/Models/DictionaryValue.php
@@ -10,36 +10,12 @@ class DictionaryValue extends Base
     // 获取某一个code字典的全部选项
     public static function GetDicOptions($code)
     {
-        $list = Redis::hGetAll('dictionary_' . $code);
-        $result = [];
-        if ($list) {
-            foreach ($list as $map) {
-                $map = json_decode($map, true);
-                if ($map['status'] == 1) {
-                    $result[] = ['value' => $map['value'], 'label' => $map['name'], 'sort' => $map['sort'] ?? 0];
-                }
-            }
-            self::sortBySort($result, 'sort');
-        }
-        return $result;
-    }
+        $list = self::select(['name as label', 'value'])
+            ->where(['status' => 1,'code'=>$code])
+            ->orderBy('sort', 'ASC')
+            ->get()
+            ->toArray();
 
-    /**
-     * 对二维数组按照指定键的值进行排序（使用usort）
-     *
-     * @param array $arr 待排序的二维数组
-     * @param string $key 指定的键名
-     * @param int $order 可选，排序顺序，1表示升序，-1表示降序
-     * @return void 直接修改原数组
-     */
-    public static function sortBySort(&$arr, $key, $order = SORT_ASC)
-    {
-        usort($arr, function ($a, $b) use ($key, $order) {
-            if (!isset($a[$key]) || !isset($b[$key])) {
-                return false;
-            }
-            $result = strcmp($a[$key], $b[$key]);
-            return ($order == SORT_ASC) ? $result : -$result;
-        });
+        return $list;
     }
 }
