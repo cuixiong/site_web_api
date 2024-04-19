7d88b324290aea4702879d2026fe6d8f4441cddf
zqy <695326913@qq.com>
2024-04-19 17:22:10 +0800

    报告列表接口语言调整

--- app/Models/Languages.php
+++ app/Models/Languages.php
@@ -1,31 +1,38 @@
 <?php
 
 namespace App\Models;
+
 use App\Models\Base;
 use Illuminate\Support\Facades\Redis;
 
 class Languages extends Base
 {
-    const RedisKey = 'Languages';
+    // const RedisKey = 'Languages';
     /**
      * 从redis中获取数据
      * 自己加风险意识代码，不想给那个SB领导写
      */
+    // public static function GetList()
+    // {
+    //     $res = [];
+    //     $lists = Redis::hgetall(self::RedisKey);
+    //     if(!empty($lists)){
+    //        foreach ($lists as $key => $value) {
+    //             $value = json_decode($value,true);
+    //             if($value['status'] == '1' ){
+    //                 $res[] = [
+    //                     'id' => $value['id'],
+    //                     'name' => $value['name'],
+    //                 ];
+    //             }
+    //        }
+    //     }
+    //     return $res;
+    // }
+
+    // 上面前同事写的，这一个是直接去数据库拿
     public static function GetList()
     {
-        $res = [];
-        $lists = Redis::hgetall(self::RedisKey);
-        if(!empty($lists)){
-           foreach ($lists as $key => $value) {
-                $value = json_decode($value,true);
-                if($value['status'] == '1' ){
-                    $res[] = [
-                        'id' => $value['id'],
-                        'name' => $value['name'],
-                    ];
-                }
-           }
-        }
-        return $res;
+        return self::select('id', 'name')->where(['status' => 1])->get()->toArray();
     }
 }
