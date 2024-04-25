c8f97810e40e7cf4bc77ecd719069bb10e8ce0bd
黑马胖纸丶 <798396652@qq.com>
2024-04-25 15:47:21 +0800

    讯搜测试

--- app/Http/Controllers/XunSearchTestController.php
+++ app/Http/Controllers/XunSearchTestController.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Common\SendEmailController;
 use App\Http\Controllers\Controller;
 use App\Http\Helper\XunSearch;
 use Illuminate\Http\Request;
+use XS;
 
 class XunSearchTestController extends Controller
 {
