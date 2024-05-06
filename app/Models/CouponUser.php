bcbdc8f5de3b9c61a231ce00692afc8f88583dcc
黑马胖纸丶 <798396652@qq.com>
2024-05-06 10:26:44 +0800

    处理反馈BUG

--- app/Models/CouponUser.php
+++ app/Models/CouponUser.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use App\Models\Base;
 
 class CouponUser extends Base {
-    const isUsedNO  = 1; //未使用
-    const isUsedYes = 0; //已使用
+    const isUsedNO  = 0; //未使用
+    const isUsedYes = 1; //已使用
 
 }
