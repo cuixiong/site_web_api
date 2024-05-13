9d29aa2140cb7d9ce78429c1a8b21eb05d2a5a73
黑马胖纸丶 <798396652@qq.com>
2024-05-13 14:36:20 +0800

    联系我们接口兼容

--- app/Http/Controllers/ContactUsController.php
+++ app/Http/Controllers/ContactUsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Common\SendEmailController;
+use App\Models\ContactUs;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\DictionaryValue;
@@ -10,51 +11,56 @@ use App\Models\MessageLanguageVersion;
 use App\Models\Country;
 use App\Models\City;
 
-class ContactUsController extends Controller
-{
-
+class ContactUsController extends Controller {
     // 免费样本/定制报告
-    public function Add(Request $request)
-    {
-        try {
-            $input = $request->all();
-            $model = new \App\Models\ContactUs();
-            $model = $model->create($input);
-            // 发送验证邮件
+    public function Add(Request $request) {
+        $params = $request->all();
+        $model = new ContactUs();
+        $model->name = $params['name'] ?? '';
+        $model->email = $params['email'] ?? '';
+        $model->company = $params['company'] ?? '';
+        $model->buy_time = $params['plan_buy_time'] ?? '';
+        $model->province_id = $params['province_id'] ?? 0;
+        $model->city_id = $params['city_id'] ?? 0;
+        $model->category_id = $params['category_id'] ?? 0;
+        $model->phone = $params['phone'] ?? '';
+        $model->content = $params['content'] ?? '';
+        $model->product_id = $params['product_id'] ?? 0;
+        if ($model->save()) {
             (new SendEmailController)->customized($model->id);
-            ReturnJson(true);
-        } catch (\Exception $e) {
-            ReturnJson(false, $e->getMessage());
+            ReturnJson(true, '', $model);
+        } else {
+            ReturnJson(false, $model->getModelError());
         }
     }
 
     // 联系我们模块字典
-    public function Dictionary(Request $request)
-    {
+    public function Dictionary(Request $request) {
         $result = [];
         // 计划购买时间
         $result['buy_time'] = DictionaryValue::GetDicOptions('Buy_Time');
-
         // 获知渠道
         $result['channel'] = DictionaryValue::GetDicOptions('Channel_Type');
-
         // 国家
-        $result['country'] = Country::where('status', 1)->select('id as value', 'data as label', 'code')->orderBy('sort', 'asc')->get()->toArray();;
-
-        $provinces = City::where(['status' => 1, 'type' => 1])->select('id as value', 'name as label')->orderBy('id', 'asc')->get()->toArray();
+        $result['country'] = Country::where('status', 1)->select('id as value', 'data as label', 'code')->orderBy(
+            'sort', 'asc'
+        )->get()->toArray();;
+        $provinces = City::where(['status' => 1, 'type' => 1])->select('id as value', 'name as label')->orderBy(
+            'id', 'asc'
+        )->get()->toArray();
         foreach ($provinces as $key => $province) {
-            $cities = City::where(['status' => 1, 'type' => 2, 'pid' => $province['value']])->select('id as value', 'name as label')->orderBy('id', 'asc')->get()->toArray();
+            $cities = City::where(['status' => 1, 'type' => 2, 'pid' => $province['value']])->select(
+                'id as value', 'name as label'
+            )->orderBy('id', 'asc')->get()->toArray();
             $provinces[$key]['children'] = $cities;
         }
         $result['city'] = $provinces;
-
         // 语言版本
         $result['language_version'] = MessageLanguageVersion::where('status', 1)
-            ->select(['name', 'id'])
-            ->orderBy('sort', 'ASC')
-            ->get()
-            ->toArray();
-
+                                                            ->select(['name', 'id'])
+                                                            ->orderBy('sort', 'ASC')
+                                                            ->get()
+                                                            ->toArray();
         ReturnJson(true, '', $result);
     }
 }
