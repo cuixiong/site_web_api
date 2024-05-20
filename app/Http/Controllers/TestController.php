0d1d0f42adc30fed21c47838ebd3c11ddf78cde3
黑马胖纸丶 <798396652@qq.com>
2024-05-20 09:59:20 +0800

    代码注释

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -17,30 +17,31 @@ use Illuminate\Support\Facades\DB;
 class TestController extends Controller {
     public function test1() {
         try{
+            $list = [];
             // 定义新的数据库配置
-            $newDatabaseConfig = [
-                'driver' => 'mysql',
-                'host' => '8.219.5.215',
-                'port' => '3306',
-                'database' => 'yadmin',
-                'username' => 'root',
-                'password' => '9d672e87bf75c4e5',
-                'charset' => 'utf8mb4',
-                'collation' => 'utf8mb4_unicode_ci',
-                'prefix' => '',
-                'strict' => true,
-                'engine' => null,
-            ];
-            // 切换到新的数据库配置
-            $mysql = "mysql";
-            Config::set("database.connections.{$mysql}", $newDatabaseConfig);
-            // 断开当前连接
-            DB::purge($mysql);
-            // 重新连接
-            DB::reconnect($mysql);
-
-            // 现在您可以使用新的数据库连接执行查询
-            $list = DB::table('users')->get();
+//            $newDatabaseConfig = [
+//                'driver' => 'mysql',
+//                'host' => '8.219.5.215',
+//                'port' => '3306',
+//                'database' => 'yadmin',
+//                'username' => 'root',
+//                'password' => '9d672e87bf75c4e5',
+//                'charset' => 'utf8mb4',
+//                'collation' => 'utf8mb4_unicode_ci',
+//                'prefix' => '',
+//                'strict' => true,
+//                'engine' => null,
+//            ];
+//            // 切换到新的数据库配置
+//            $mysql = "mysql";
+//            Config::set("database.connections.{$mysql}", $newDatabaseConfig);
+//            // 断开当前连接
+//            DB::purge($mysql);
+//            // 重新连接
+//            DB::reconnect($mysql);
+//
+//            // 现在您可以使用新的数据库连接执行查询
+//            $list = DB::table('users')->get();
 
             dd($list);
         }catch (\Exception $e){
