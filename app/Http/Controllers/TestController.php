d2e651630347bc39012d825e3a8433f8adc42932
黑马胖纸丶 <798396652@qq.com>
2024-05-20 09:51:57 +0800

    测试 新加坡数据库连接 是否是通的

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -15,12 +15,31 @@ use Illuminate\Support\Facades\DB;
 
 class TestController extends Controller {
     public function test1() {
-        $connection = DB::connection('default');
-        $connection->setHost('8.219.5.215');
-        $connection->setDatabaseName('yadmin');
-        $connection->setUsername('root');
-        $connection->setPassword('9d672e87bf75c4e5');
-        $list = DB::connection('default')->table('users')->get();
+        // 定义新的数据库配置
+        $newDatabaseConfig = [
+            'driver' => 'mysql',
+            'host' => 'new_host',
+            'port' => '3306',
+            'database' => 'yadmin',
+            'username' => 'root',
+            'password' => '9d672e87bf75c4e5',
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
+        ];
+        // 切换到新的数据库配置
+        $mysql = "mysql";
+        Config::set("database.connections.{$mysql}", $newDatabaseConfig);
+        // 断开当前连接
+        DB::purge($mysql);
+        // 重新连接
+        DB::reconnect($mysql);
+
+        // 现在您可以使用新的数据库连接执行查询
+        $list = DB::table('users')->get();
+
         dd($list);
     }
 
