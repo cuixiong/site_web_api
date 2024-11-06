e3d4be4fbfdcf22dc41ec32e99afcc1a18e233db
黑马胖纸丶 <798396652@qq.com>
2024-11-06 09:27:52 +0800

    1. 云上支付相关修改

--- app/Http/Controllers/Pay/AirwallexPay.php
+++ app/Http/Controllers/Pay/AirwallexPay.php
@@ -34,6 +34,8 @@ class AirwallexPay extends Pay {
         $this->apiUrl = env('airwallex_api_url', 'https://api-demo.airwallex.com');
 
         $this->env = env('airwallex_env', 'demo');
+
+        \Log::error('返回结果数据:'.json_encode([$this->apiUrl, $this->client_id , $this->secret_id , $this->webhook_id, $this->env]).'  文件路径:'.__CLASS__.'  行号:'.__LINE__);
     }
 
     public function createFormdata($order) {
@@ -103,12 +105,17 @@ class AirwallexPay extends Pay {
     public function do($order, $options = []) {
         $domain = rtrim(env('APP_URL', ''), '/');
         $returnUrl = $domain.'/paymentcomplete/'.$order->id; // 同步回调地址
+        if($this->env == 'demo'){
+            $isProduction = false;
+        }else{
+            $isProduction = true;
+        }
         $config = [
             'clientId'   => $this->client_id,
             'apiKey'     => $this->secret_id,
-            'production' => false,
+            'production' => $isProduction,
         ];
-        $airwallex = new \Jitoot\Airwallex\Client($config);
+        $airwallex = new Client($config);
         $orderNumber = $order['order_number'];
         $payData = [
             'amount'            => $order->actually_paid,
@@ -121,7 +128,8 @@ class AirwallexPay extends Pay {
         // 后面托管页面展示描述，在此我填写报告名称 长度32
         //$orderGoodsName = $order->getProductNameAttribute();
         //$payData['descriptor'] = $orderGoodsName;
-        list($code, $resData) = $airwallex->paymentIntent->create($payData);
+        list($code, $resData) = $airwallex->request('POST', '/pa/payment_intents/create', $payData);
+
         if (!empty($code) && $code == 201) {
             $intent_id = $resData->id;
             $client_secret = $resData->client_secret;
@@ -210,3 +218,114 @@ class AirwallexPay extends Pay {
         return $result;
     }
 }
+
+
+class Client {
+    const API_URL = 'https://api.airwallex.com/api/v1';
+    const TEST_API_URL = 'https://api-demo.airwallex.com/api/v1';
+    private $apiKey;
+    private $clientId;
+    private $config;
+
+    public function __construct($config = []) {
+        $this->config = [
+            'production' => false, //test or prod environment?
+        ];
+        foreach ($config as $key => $item) {
+            switch ($key) {
+                case 'clientId':
+                    $this->clientId = $item;
+                    break;
+                case 'apiKey':
+                    $this->apiKey = $item;
+                    break;
+                default:
+                    $this->config[$key] = $item;
+                    break;
+            }
+        }
+    }
+
+    //execute curl
+    public function request($method, $path, $params = []) {
+        $token = $this->authenticate();
+        $headers = [
+            'Authorization' => $token
+        ];
+        $response = $this->curl($method, $path, $headers, $params);
+
+        return $response;
+    }
+
+    protected function getApiUrl() {
+        return (true === $this->config['production'] ? self::API_URL : self::TEST_API_URL);
+    }
+
+    protected function authenticate() {
+        //prepare credentials (login)
+        $headers = [
+            'x-client-id' => $this->getClientId(),
+            'x-api-key'   => $this->getApiKey(),
+        ];
+        list($code, $response) = $this->curl('POST', '/authentication/login', $headers);
+        if (!$response->token) {
+            throw new \Exception($response->message);
+        }
+
+        return $response->token;
+    }
+
+    //curl wrapper
+    protected function curl($method, $url, $headers = [], $params = []) {
+        $headers = array_merge(['Content-Type' => 'application/json'], $headers);
+        $curlHeaders = [];
+        foreach ($headers as $key => $header) {
+            if ('Authorization' === $key) {
+                $curlHeaders[] = "$key: Bearer ".$header;
+            } else {
+                $curlHeaders[] = "$key: ".$header;
+            }
+        }
+        $curl = curl_init();
+        switch ($method) {
+            case "POST":
+                curl_setopt($curl, CURLOPT_POST, 1);
+                $params = json_encode($params);
+                curl_setopt($curl, CURLOPT_POSTFIELDS, $params);
+                break;
+            case "GET":
+                if ($params['id']) {
+                    $url = $url.'/'.$params['id'];
+                }
+                unset($params['id']);
+                if (count($params) > 0) {
+                    $url = $url.'?'.http_build_query($params);
+                }
+                break;
+        }
+        curl_setopt($curl, CURLOPT_URL, $this->getApiUrl().$url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $curlHeaders);
+        $result = json_decode(curl_exec($curl));
+        $code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+
+        return [$code, $result];
+    }
+
+    /*
+     * @return string the API key used for requests
+     */
+    private function getApiKey() {
+        return $this->apiKey;
+    }
+
+    /*
+     * @return string the client_id used for requests
+     */
+    private function getClientId() {
+        return $this->clientId;
+    }
+}
+
+
+
