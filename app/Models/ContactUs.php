af944da55db3d989c79b5eb1888b0a20b22acd41
Michael Li <798396652@qq.com>
2025-07-08 14:03:57 +0800

    refactor(app): 关于我们功能迭代

--- app/Models/ContactUs.php
+++ app/Models/ContactUs.php
@@ -4,26 +4,30 @@ namespace App\Models;
 
 use App\Models\Base;
 
-class ContactUs extends Base
-{
+class ContactUs extends Base {
     protected $table = 'contact_us';
-    protected $fillable = [
-        'category_id',
-        'product_id',
-        'name',
-        'phone',
-        'email',
-        'company',
-        'content',
-        'country_id',
-        'province_id',
-        'city_id',
-        'buy_time',
-        'channel',
-        'channel_name',
-        'price_edition',
-        'language_version',
-        'address',
-        'department',
-    ];
+    protected $fillable
+                     = [
+            'category_id',
+            'product_id',
+            'name',
+            'phone',
+            'email',
+            'company',
+            'content',
+            'country_id',
+            'province_id',
+            'city_id',
+            'buy_time',
+            'channel',
+            'channel_name',
+            'price_edition',
+            'language_version',
+            'address',
+            'department',
+            'ua_info',
+            'ua_browser_name',
+            'referer',
+            'referer_alias_id',
+        ];
 }
