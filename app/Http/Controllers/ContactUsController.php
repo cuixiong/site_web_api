5cc29b91638ea71ceb1f5f77639337db5c600125
Michael Li <798396652@qq.com>
2025-08-04 10:23:28 +0800

    refactor(ContactUsController): 优化浏览器名称识别逻辑

--- app/Http/Controllers/ContactUsController.php
+++ app/Http/Controllers/ContactUsController.php
@@ -90,7 +90,7 @@ class ContactUsController extends Controller {
         $model->referer = $HTTP_REFERER;
         $alias_id = $this->getAliasId($HTTP_REFERER, $model);
         $model->referer_alias_id = $alias_id;
-        
+
         $copyModel = clone $model;
         $model->product_id = $productId;
         $model->price_edition = $priceEdition;
@@ -98,7 +98,7 @@ class ContactUsController extends Controller {
         if (!$model->save()) {
             ReturnJson(false, $model->getModelError());
         }
-        // 
+        //
         $saveIds = [];
         if(count($otherProductIds)>0){
             foreach ($otherProductIds as $key => $otherProductId) {
@@ -200,10 +200,11 @@ class ContactUsController extends Controller {
     public function getBrowserName($ua_info) {
         // 浏览器特征正则表达式库
         $browserPatterns = [
+            'iOS'        => '/(iPhone|iPad|iPod)/i',          // iOS设备检测（优先级最高）
+            'safari'     => '/safari\/[\d\.]+/i',            // 标准Safari浏览器
             'edge'       => '/edg\/[\d\.]+/i',
             'chrome'     => '/chrome\/[\d\.]+/i',
             'firefox'    => '/firefox\/[\d\.]+/i',
-            'safari'     => '/safari\/[\d\.]+/i',
             'opr'        => '/opr\/[\d\.]+/i',    // Opera 新版标识
             'opr_legacy' => '/opera\/[\d\.]+/i',  // Opera 旧版标识
             'ie'         => '/trident/i',          // IE 11+ 特征
